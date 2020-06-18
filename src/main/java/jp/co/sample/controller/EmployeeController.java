8c9f310b2d03a394fada9650d0f9a40bb13a8d35
akaseren <ren.akase@rakus.co.jp>
2020-06-18 10:24:43 +0900

    従業員詳細機能作成
    　list.html作成
    
    　EmployeeService作成
    
    EmployeeController,EmployeeRepository修正,detail.html作成

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -1,5 +1,69 @@
 package jp.co.sample.controller;
 
-public class EmployeeController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
+import jp.co.sample.service.EmployeeService;
+
+/**
+ * 従業員用コントローラ.
+ * 
+ * @author ren.akase
+ *
+ */
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+	
+	/**
+	 * フォームをインスタンス化.
+	 * 
+	 * @return フォーム
+	 */
+	@ModelAttribute
+	public UpdateEmployeeForm setUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+
+	@Autowired
+	private EmployeeService employeeService;
+	
+	/**
+	 * 従業員情報一覧を取得.
+	 * 
+	 * @param model: モデル
+	 * @return　従業員一覧画面
+	 */
+	@RequestMapping("/showList")
+	public String showList(Model model) {
+		List<Employee> employeeList = employeeService.showList();
+		model.addAttribute("employeeList", employeeList);
+		
+		return "/employee/list";
+	}
+	
+	/**
+	 * 従業員詳細を取得.
+	 * 
+	 * @param id:ID
+	 * @param model:モデル
+	 * @return　従業員詳細画面
+	 */
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model) {
+		Employee employee = employeeService.showDetail(Integer.parseInt(id));
+		
+		model.addAttribute("employee", employee);
+		
+		
+		return "employee/detail";
+	}
+	
 }
--- src/main/java/jp/co/sample/repository/EmployeeRepository.java
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -32,10 +32,12 @@ public class EmployeeRepository {
 		employee.setGender(rs.getString("gender"));
 		employee.setHireDate(rs.getDate("hire_date"));
 		employee.setMailAddress(rs.getString("mail_address"));
+		employee.setZipCode(rs.getString("zip_code"));
+		employee.setAddress(rs.getString("address"));
 		employee.setTelephone(rs.getString("telephone"));
 		employee.setSalary(rs.getInt("salary"));
 		employee.setCharacteristics(rs.getString("characteristics"));
-		employee.setDependentsCount(rs.getInt("dependentsCount"));
+		employee.setDependentsCount(rs.getInt("dependents_count"));
 		
 		return employee;
 	};
